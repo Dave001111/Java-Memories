43e19ab724e09745455aa5c70dc92e366dc5e75e
David <David>
2026-06-19 12:46:55 +0100

    Add marun.java file

---
+++ Marun.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class Marun {
+  public static void main(String[] args) {
+
+
+ Scanner input = new Scanner(System.in);
+ 
+ int[] value = new int[5];
+
+ System.out.print("Enter a number: ");
+ value[0] = input.nextInt();
+
+ System.out.print("Enter a number: ");
+ value[1] = input.nextInt();
+
+ System.out.print("Enter a number: ");
+ value[2] = input.nextInt();
+
+ System.out.print("Enter a number: ");
+ value[3] = input.nextInt();
+
+ System.out.print("Enter a number: ");
+ value[4] = input.nextInt();
+
+System.out.print(value[0] + ", ");
+System.out.print(value[1] + ", ");
+System.out.print(value[2] + ", ");
+System.out.print(value[3] + ", ");
+System.out.print(value[4] + " ");
+
+
+ 
+
+
+  }
+
+}
